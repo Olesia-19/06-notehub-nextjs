8f0d78ec81c36ed24e7e155d643f32cbe3c0361a
olesia <ldsolesya@gmail.com>
2025-08-04 10:52:57 +0100

    edit TanStackProvider folder name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from '@/components/Header/Header';
+import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import TanstackProvider from "@/components/TanstackProvider/TanstackProvider";
+import TanstackProvider from "@/components/TanStackProvider/TanstackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,8 +30,8 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <TanstackProvider>
           <Header />
-        {children}
-        <Footer/>
+          {children}
+          <Footer />
         </TanstackProvider>
       </body>
     </html>
