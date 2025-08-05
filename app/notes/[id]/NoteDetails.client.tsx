b3b2117d09db25a70fb690a802a92c4e255ce8bd
olesia <ldsolesya@gmail.com>
2025-08-05 23:23:02 +0100

    edit NoteDetails.client.tsx

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -4,24 +4,33 @@ import { getSingleNote } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import css from "@/app/notes/Notes.module.css";
+import Loader from "@/components/Loader/Loader";
 
 const NoteDetailsPageClient = () => {
   const { id } = useParams<{ id: string }>();
 
-  const { data: note } = useQuery({
+  const {
+    data: note,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["note", id],
     queryFn: () => getSingleNote(id),
     refetchOnMount: false,
   });
 
+  if (isLoading) return <Loader />;
+
+  if (isError || !note) return <p>Something went wrong.</p>;
+
   return (
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>{note?.title}</h2>
+          <h2>{note.title}</h2>
         </div>
-        <p className={css.content}>{note?.content}</p>
-        <p className={css.date}>{note?.createdAt}</p>
+        <p className={css.content}>{note.content}</p>
+        <p className={css.date}>{note.createdAt}</p>
       </div>
     </div>
   );
